7f27ea38aecbbf0e6d1df2bd0fe77d041aab094c
Or Abitbul <osom445@gmail.com>
2021-07-16 02:43:26 +0300

    wip

---
+++ server/server-common/build/topics/user-topics.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _default = {
+  newUser: 'new.user',
+  updateUser: 'update.user',
+  deleteUser: 'delete.user'
+};
+exports["default"] = _default;
--- server/services/user/src/index.js
+++ server/services/user/src/index.js
@@ -3,6 +3,9 @@ import app from './config/app';
 import connectMongoose from './config/mongodb';
 import connectRabbit from './config/rabbitmq';
 import subscribe from './jobs';
+import { userTopics } from '@orab/rabbitmq-server-common';
+
+console.log(userTopics);
 
 connectMongoose()
     .then(() => app()
